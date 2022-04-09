5d044c3e73d339b9bf05b4db06d401ca9ef395eb
Derick Young <young.derickj@gmail.com>
2022-04-09 17:03:56 -0600

    Add endpoints

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,4 +6,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/home', function(req, res, next) {
+  res.send({Home: 'Homepage'})
+});
+
+router.get('/categories/:name', function(req, res, next) {
+  const name = req.params.name;
+  if(name=="food"){
+    
+    res.send("For food");
+  }
+  if(name=="clothing"){
+    
+    res.send("For clothing");
+  }
+  if(name=="recreation"){
+    
+    res.send("For recreation");
+  }
+  if(name=="autoService"){
+    
+    res.send("For auto service");
+  }
+});
+
+router.get('/categories/:name/:business', function(req, res, next) {
+  res.send('business')
+});
+
 module.exports = router;
