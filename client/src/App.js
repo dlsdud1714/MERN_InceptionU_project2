5990217d90018f76b344183bfc25d7fd9e5df337
Derick Young <young.derickj@gmail.com>
2022-04-09 17:09:23 -0600

    Add myClick

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  // const [home ,setHome] = useState();
+  const myClick = async () => {
+    let response = await fetch("/home");
+    //let res = await response.json()
+    console.log(response);
+    return response;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={myClick}>botton</button>
     </div>
   );
 }
