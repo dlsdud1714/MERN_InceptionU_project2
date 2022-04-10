9a13af277cc5f5319116902e238d4ddddb3fa267
Inyoung Park <rkduddl1rl@gmail.com>
2022-04-09 19:20:56 -0600

    homepage error fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  // const [home ,setHome] = useState();
+  const [home ,setHome] = useState();
+
   const myClick = async () => {
     let response = await fetch("/home");
-    //let res = await response.json()
+    let res = await response.json();
     console.log(response);
-    return response;
+    console.log(res);
+    setHome(res.Home);
   };
 
   return (
     <div className="App">
       <button onClick={myClick}>botton</button>
+    <p>{home}</p>
     </div>
   );
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,37 +1,33 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
-router.get('/home', function(req, res, next) {
-  res.send({Home: 'Homepage'})
+router.get("/home", function (req, res, next) {
+  res.send({ Home: "Homepage" });
 });
 
-router.get('/categories/:name', function(req, res, next) {
+router.get("/categories/:name", function (req, res, next) {
   const name = req.params.name;
-  if(name=="food"){
-    
+  if (name == "food") {
     res.send("For food");
   }
-  if(name=="clothing"){
-    
+  if (name == "clothing") {
     res.send("For clothing");
   }
-  if(name=="recreation"){
-    
+  if (name == "recreation") {
     res.send("For recreation");
   }
-  if(name=="autoService"){
-    
+  if (name == "autoService") {
     res.send("For auto service");
   }
 });
 
-router.get('/categories/:name/:business', function(req, res, next) {
-  res.send('business')
+router.get("/categories/:name/:business", function (req, res, next) {
+  res.send("business");
 });
 
 module.exports = router;
