754831a271aab80af9913fe76b2d668429bd7f8f
Inyoung Park <rkduddl1rl@gmail.com>
2022-04-17 03:36:30 -0600

    connect to local db

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 
+const connectionString = 'mongodb://localhost:27017'
+// process.env.MONGODB_URI;
 
-const connectionString = process.env.MONGODB_URI;
-  console.log(process.env)
 mongoose.connect(connectionString, () => {
   console.log("connected to mongoose");
 });
