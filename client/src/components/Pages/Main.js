2faa4974f18a98e556f532989cf7ad2bd11b17ef
Inyoung Park <rkduddl1rl@gmail.com>
2022-04-17 11:57:20 -0600

    Get data to main page

--- client/src/components/Pages/Main.js
+++ client/src/components/Pages/Main.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Category from '../Category';
+import Gps from '../Gps';
+import NavBar from '../NavBar';
+import SearchBar from '../SearchBar';
 
-const Main = () => {
+const Main = (props) => {
+  const {allData} = props
+  console.log(allData)
+   const [home ,setHome] = useState();
+
+  // const myClick = async () => {
+  //   let response = await fetch("/home");
+  //   let res = await response.json();
+  //   console.log(response);
+  //   console.log(res);
+  //   setHome(res.Home);
+  // };
+
   return (
-    <div>Main</div>
-  )
+    <div className="main">
+    <NavBar />
+    <SearchBar />
+    <Category />
+    <Gps />
+      {/* <button onClick={myClick}>button</button>
+    <p>{home}</p> */}
+    </div>
+  );
 }
 
 export default Main
