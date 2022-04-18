1f0d5d3ecc66939089bf07329392675b49d8e71a
Nicole <nicoleamdubois@hotmail.com>
2022-04-18 15:14:38 -0600

    Adding react style

---
+++ client/src/components/BusinessCard.js
@@ -0,0 +1,5 @@
+{/* <Card/>
+<Box/>
+<Header/>
+<Text/>
+<Button/> */}
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,29 @@
 import React from 'react'
 
-export const Header = () => {
+// export const Header = () => {
+//   return (
+//     <div>Header</div>
+//   )
+// }
+
+const Header = () => {
+  const myStyle = {
+    color: "white",
+    backgroundColor: "DodgerBlue",
+    border: "1px solid black",
+    paddingtop: "50px",
+    paddingright: "30px",
+    paddingbottom: "50px",
+    paddingleft: "80px",
+    fontFamily: "Sans-Serif",
+  };
   return (
-    <div>Header</div>
-  )
+    <>
+      <h1 style={myStyle}>YYCWhatsNearMe?</h1>
+    </>
+  );
 }
+
+export default Header;
+
+
