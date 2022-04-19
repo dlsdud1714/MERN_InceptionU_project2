b540a8509872b422be79a201799566b0f6530f57
Nicole <nicoleamdubois@hotmail.com>
2022-04-19 14:30:56 -0600

    Co-authored-by: InyoungPark <dlsdud1714@users.noreply.github.com> Co-authored-by: Derick Young <dericky1@users.noreply.github.com>

--- server/db/loadInitialData.js
+++ server/db/loadInitialData.js
@@ -1,31 +1,21 @@
-const initialDataAuto = require("./data/auto.json");
-const initialDataBeauty = require("./data/beauty.json");
-const initialDataCafe = require("./data/cafe.json");
-const initialDataClothing = require("./data/clothing.json");
-const initialDataFastFood = require("./data/fastfood.json");
-const initialDataGroceries = require("./data/Groceries.json");
-const initialDataPets = require("./data/Pets.json");
-const initialDataRecreation = require("./data/Recreation.json");
-const initialDataRestaurant = require("./data/Restaurant.json");
+// const initialDataAuto = require("./data/auto.json");
+// const initialDataBeauty = require("./data/beauty.json");
+// const initialDataCafe = require("./data/cafe.json");
+// const initialDataClothing = require("./data/clothing.json");
+// const initialDataFastFood = require("./data/fastfood.json");
+const initialData = require("./data/localbusinesses.json");
+// petsBusinesses,
+// recreationBusinesses,
+// restaurantBusinesses} 
+const {createLocalBusiness, localBusinesses} = require("./models/localBusinessModel.js")
 
-
-const {createLocalBusiness, 
-    autoBusinesses, 
-    cafeBusinesses, 
-    beautyBusinesses, 
-    clothingBusinesses, 
-    fastFoodBusinesses,
-    groceriesBusinesses,
-    petsBusinesses,
-    recreationBusinesses,
-    restaurantBusinesses} = require("./models/localBusinessModel.js");
-
-initialDataAuto.map((data)=>createLocalBusiness(data, autoBusinesses));
-initialDataBeauty.map((data)=>createLocalBusiness(data, beautyBusinesses));
-initialDataCafe.map((data)=>createLocalBusiness(data, cafeBusinesses));
-initialDataClothing.map((data)=>createLocalBusiness(data, clothingBusinesses));
-initialDataFastFood.map((data)=>createLocalBusiness(data, fastFoodBusinesses));
-initialDataGroceries.map((data)=>createLocalBusiness(data, groceriesBusinesses));
-initialDataPets.map((data)=>createLocalBusiness(data, petsBusinesses));
-initialDataRecreation.map((data)=>createLocalBusiness(data, recreationBusinesses));
-initialDataRestaurant.map((data)=>createLocalBusiness(data, restaurantBusinesses));
+// initialDataAuto.map((data)=>createLocalBusiness(data, autoBusinesses));
+// initialDataBeauty.map((data)=>createLocalBusiness(data, beautyBusinesses));
+// initialDataCafe.map((data)=>createLocalBusiness(data, cafeBusinesses));
+// initialDataClothing.map((data)=>createLocalBusiness(data, clothingBusinesses));
+// initialDataFastFood.map((data)=>createLocalBusiness(data, fastFoodBusinesses));
+// initialDataGroceries.map((data)=>createLocalBusiness(data, groceriesBusinesses));
+// initialDataPets.map((data)=>createLocalBusiness(data, petsBusinesses));
+// initialDataRecreation.map((data)=>createLocalBusiness(data, recreationBusinesses));
+// initialDataRestaurant.map((data)=>createLocalBusiness(data, restaurantBusinesses));
+initialData.map((data)=>createLocalBusiness(data, localBusinesses));
--- server/db/models/localBusinessModel.js
+++ server/db/models/localBusinessModel.js
@@ -22,21 +22,23 @@ const localBusinessSchema = new Schema({
   neighbourhood: String,
   jobStatus: String,
   jobCreated: String,
+  headCategory: String
 });
 
-const autoBusinesses = mongoose.model("auto", localBusinessSchema);
-const cafeBusinesses = mongoose.model("coffeshop", localBusinessSchema);
-const beautyBusinesses = mongoose.model("beauty", localBusinessSchema);
-const clothingBusinesses = mongoose.model("clothing", localBusinessSchema);
-const fastFoodBusinesses = mongoose.model("fastfood", localBusinessSchema);
-const groceriesBusinesses = mongoose.model("groceries", localBusinessSchema);
-const petsBusinesses = mongoose.model("pets", localBusinessSchema);
-const recreationBusinesses = mongoose.model("recreation", localBusinessSchema);
-const restaurantBusinesses = mongoose.model("restaurant", localBusinessSchema);
+// const autoBusinesses = mongoose.model("auto", localBusinessSchema);
+// const cafeBusinesses = mongoose.model("coffeshop", localBusinessSchema);
+// const beautyBusinesses = mongoose.model("beauty", localBusinessSchema);
+// const clothingBusinesses = mongoose.model("clothing", localBusinessSchema);
+// const fastFoodBusinesses = mongoose.model("fastfood", localBusinessSchema);
+// const groceriesBusinesses = mongoose.model("groceries", localBusinessSchema);
+// const petsBusinesses = mongoose.model("pets", localBusinessSchema);
+// const recreationBusinesses = mongoose.model("recreation", localBusinessSchema);
+// const restaurantBusinesses = mongoose.model("restaurant", localBusinessSchema);
+const localBusinesses = mongoose.model("businesses", localBusinessSchema);
 
 const createLocalBusiness = async (data, name) => {
   const newLocalBusiness = await name.create(data);
-  // console.log('created new local business:', newLocalBusiness)
+// console.log('created new local business:', newLocalBusiness)
   return newLocalBusiness;
 };
 
@@ -45,45 +47,31 @@ const findAllLocalBusiness = async (name) => {
   return findCategory;
 };
 
-const findCafe =async() =>{
-  const findCafeData = await cafeBusinesses.find();
-  return findCafeData
-}
+// const categoryArray = [
+//   autoBusinesses,
+//   cafeBusinesses,
+//   beautyBusinesses,
+//   clothingBusinesses,
+//   fastFoodBusinesses,
+//   restaurantBusinesses,
+//   groceriesBusinesses,
+//   petsBusinesses,
+//   recreationBusinesses,
+// ];
 
-const categoryArray = [
-  autoBusinesses,
-  cafeBusinesses,
-  beautyBusinesses,
-  clothingBusinesses,
-  fastFoodBusinesses,
-  restaurantBusinesses,
-  groceriesBusinesses,
-  petsBusinesses,
-  recreationBusinesses,
-];
 
-const array = [1, 2, 3, 4, 5]
-
-// const data = categoryArray.map((category) => {
-//   findAllLocalBusiness(category)//.then((data) => console.log("Data", data))
-// });
-const result = []
-const data = array.map((index) => {
-  const value= index + 1;
-  result.push(value);
-});
-console.log("Example", result)
 
 module.exports = {
   createLocalBusiness,
-  findAllLocalBusiness, data, findCafe,
-  autoBusinesses,
-  cafeBusinesses,
-  beautyBusinesses,
-  clothingBusinesses,
-  fastFoodBusinesses,
-  groceriesBusinesses,
-  petsBusinesses,
-  recreationBusinesses,
-  restaurantBusinesses,
+  findAllLocalBusiness,
+  localBusinesses
 };
+  // autoBusinesses,
+  // cafeBusinesses,
+  // beautyBusinesses,
+  // clothingBusinesses,
+  // fastFoodBusinesses,
+  // groceriesBusinesses,
+  // petsBusinesses,
+  // recreationBusinesses,
+  // restaurantBusinesses,
