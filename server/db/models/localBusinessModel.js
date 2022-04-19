6a099d7ca76bb8eb3fdd3d03c7161c422215cee4
Derick Young <young.derickj@gmail.com>
2022-04-19 15:01:17 -0600

    debugging

--- server/db/models/localBusinessModel.js
+++ server/db/models/localBusinessModel.js
@@ -25,15 +25,6 @@ const localBusinessSchema = new Schema({
   headCategory: String
 });
 
-// const autoBusinesses = mongoose.model("auto", localBusinessSchema);
-// const cafeBusinesses = mongoose.model("coffeshop", localBusinessSchema);
-// const beautyBusinesses = mongoose.model("beauty", localBusinessSchema);
-// const clothingBusinesses = mongoose.model("clothing", localBusinessSchema);
-// const fastFoodBusinesses = mongoose.model("fastfood", localBusinessSchema);
-// const groceriesBusinesses = mongoose.model("groceries", localBusinessSchema);
-// const petsBusinesses = mongoose.model("pets", localBusinessSchema);
-// const recreationBusinesses = mongoose.model("recreation", localBusinessSchema);
-// const restaurantBusinesses = mongoose.model("restaurant", localBusinessSchema);
 const localBusinesses = mongoose.model("businesses", localBusinessSchema);
 
 const createLocalBusiness = async (data, name) => {
@@ -47,31 +38,13 @@ const findAllLocalBusiness = async (name) => {
   return findCategory;
 };
 
-// const categoryArray = [
-//   autoBusinesses,
-//   cafeBusinesses,
-//   beautyBusinesses,
-//   clothingBusinesses,
-//   fastFoodBusinesses,
-//   restaurantBusinesses,
-//   groceriesBusinesses,
-//   petsBusinesses,
-//   recreationBusinesses,
-// ];
 
-
-
+const filterData = async (data) => {
+  data.filter(data.id
+}
 module.exports = {
   createLocalBusiness,
   findAllLocalBusiness,
   localBusinesses
 };
-  // autoBusinesses,
-  // cafeBusinesses,
-  // beautyBusinesses,
-  // clothingBusinesses,
-  // fastFoodBusinesses,
-  // groceriesBusinesses,
-  // petsBusinesses,
-  // recreationBusinesses,
-  // restaurantBusinesses,
+
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 require('dotenv').config({path: '../.env'})
 
 
-const connectionString = "mongodb+srv://dlsdud1714:in2005125mb@localbusiness.ponup.mongodb.net/localBusiness?retryWrites=true&w=majority"
+const connectionString = 'mongodb+srv://dlsdud1714:in2005125mb@localbusiness.ponup.mongodb.net/localBusiness?retryWrites=true&w=majority'
 //process.env.MONGODB_URI;
-  console.log(process.env)
+//console.log(process.env)
 mongoose.connect(connectionString, () => {
   console.log(`connected to mongoose on ${connectionString}`);
 });
