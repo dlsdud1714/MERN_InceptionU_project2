f7ba2440356b025797035ff25a16521581127d35
Inyoung Park <rkduddl1rl@gmail.com>
2022-04-18 22:01:02 -0600

    Footer added

--- client/src/components/Pages/Main.js
+++ client/src/components/Pages/Main.js
@@ -5,17 +5,19 @@ import SearchBar from "../SearchBar";
 import '../../test.css'
 import NavBar from "../NavBar";
 import Header from "../Header";
+import Footer from "../Footer";
 
 
 const Main = () => {
   const [categoryString, setCategoryString] = useState(categoryNames);
 
   return (
-    <div>
+    <div className="main">
       <Header />
       {/* <NavBar/> */}
       <SearchBar/>
       <Category categories={categoryString}/>
+      <Footer/>
   </div>
   )
 };
