6f04ecd050dbbd32e7fc154eedc75be325cd23af
Inyoung Park <rkduddl1rl@gmail.com>
2022-04-21 21:05:19 -0600

    Connected category icon to list page

--- client/src/components/Category.js
+++ client/src/components/Category.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+
 
 const Category = (props) => {
   const { categories } = props;
+  const navigate =useNavigate();
 
   const listGenerator = () => {
     return categories.map((category, index) => {
       return (
-        <button className="category" key={`category ${index}`}>
+        <button className="category" onClick={()=>navigate(`/categories/${category.headCategory}`)} key={`category ${index}`}>
           <i className={category.icon}></i>
           <span>{category.name}</span>
         </button>
