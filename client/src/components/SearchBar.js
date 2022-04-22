2871cb4cf7f6d71afe5d673404e3834dd95c8784
Derick Young <young.derickj@gmail.com>
2022-04-22 16:57:03 -0600

    Update SearchBar

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,40 +1,53 @@
-
-import { Button } from "bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { useState } from "react";
-import {Link} from 'react-router-dom';
+import "../App.css";
 
+const SearchBarDropDown = (props) => {
+  const val = props.val;
+  return (
+    <div className="search-bar-dropdown">
+      <div class="list-group">
+        <a href={`/business/${val._id}`} className="list-group-item list-group-item-action">
+            {val.title}
+        </a>
+      </div>
+    </div>
+
+  );
+};
 
 const SearchBar = (props) => {
   // console.log("props is:", props)
   // console.log("businessData is:", businessData)
   const [searchTerm, setSearchTerm] = useState("");
+  const businessData = props.businessData;
   return (
-    <div className="SearchBar">
-      <input
-        type="text"
-        placeholder="Search"
-        onChange={(event) => setSearchTerm(event.target.value)}
-      />
-      {props.businessData&&props.businessData
+    <div className="search-bar-dropdown">
+      <input type="text" class="form-control" placeholder="Search" onChange={(event) => setSearchTerm(event.target.value)} />
+      {businessData &&
+        businessData
         .filter((value) => {
-          if (searchTerm == "")
-            return false
-          else if (value?.title?.toLowerCase().includes(searchTerm.toLowerCase()))
-            return true
-        })
-        .map((val) => {
-          return <Link to= {`/business/${val._id}`}> 
-                    <ul class = "list-group">
-                      <a href={`/business/${val._id}`}> {val.title}</a>
-                    </ul> 
-                 </Link>;
-        })}
+          if (searchTerm === "") return false;
+          else if (
+            value?.title?.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+            return true;
+          })
+          .map((val) => {
+            return (<SearchBarDropDown  val = {val}/>
+            );
+          })}
     </div>
   );
 };
 
+//import { Link } from "react-router-dom";
+// <Link to={`/business/${val._id}`}>
+//   <ul className="searchResult">
+//     <a href={`/business/${val._id}`}> {val.title}</a>
+//   </ul>
+// </Link>
 // use .filter() to filter data
 // then .map() to render the renaming entries in the array
 // used useState to set and rerender the page when the searchTerm is updated
