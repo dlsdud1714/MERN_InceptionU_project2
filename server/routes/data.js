0bcdc9110bca4d340fedb29dd4ca16b3eec7453a
Inyoung Park <rkduddl1rl@gmail.com>
2022-04-23 02:19:08 -0600

    Editor - create first post in mongoDb

--- server/routes/data.js
+++ server/routes/data.js
@@ -1,13 +1,16 @@
 var express = require("express");
-const {createPosts, findBusinessPosts, updataBusinessPosts} = require('../db/models/businessPostsModel')
-const uploadImg = require("../controller/dataController")
+const {
+  createPosts,
+  findBusinessPosts,
+  updataBusinessPosts,
+} = require("../db/models/businessPostsModel");
+const uploadImg = require("../controller/dataController");
 
 var router = express.Router();
 const {
   findAllLocalBusiness,
   localBusinesses,
 } = require("../db/models/localBusinessModel");
-
 
 router.get("/", async (req, res) => {
   try {
@@ -18,37 +21,43 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.post("/img",uploadImg.single('img') ,(req, res) => {
+router.post("/img", uploadImg.single("img"), (req, res) => {
   try {
-      const IMG_URL = `/img/${req.file.filename}`;
-    res.json({url:IMG_URL})
+    const IMG_URL = `/img/${req.file.filename}`;
+    res.json({ url: IMG_URL });
   } catch (err) {
     console.log(err);
   }
 });
 
-router.post("/businessPosts", async(req,res)=>{
-    try{
-        console.log("response", req.body)
-          const update = await updataBusinessPosts(req.body.businessId,req.body.data.postId,req.body.data)
-       if(update.matchedCount===0){
-         await createPosts(req.body);
-       } 
-        res.json({status:"success", data: req.body})
-    }catch(err){
-        console.log(err);
+router.post("/businessPosts", async (req, res) => {
+  try {
+    console.log("response", req.body);
+    if (await (await findBusinessPosts(req.body.businessId)).length!==0) {
+      console.log("What is ",await findBusinessPosts(req.body.businessId))
+      const update = await updataBusinessPosts(
+        req.body.businessId,
+        req.body.data.postId,
+        req.body.data
+      );
+      console.log("return value", update);
+    } else {
+      await createPosts(req.body);
     }
+    res.json({ status: "success", data: req.body });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.get("/businessPosts/:id", async(req,res)=>{
-    try{
-        const id= req.params.id;
-        const businessData = await findBusinessPosts(id);
-        res.json({status:"success", data: businessData})
-    }catch(err){
-        console.log(err)
-    }
-})
-
+router.get("/businessPosts/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const businessData = await findBusinessPosts(id);
+    res.json({ status: "success", data: businessData });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
