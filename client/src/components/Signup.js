be4bdaddde737a0f3bf7adb0182250cb6a977351
Inyoung Park <rkduddl1rl@gmail.com>
2022-04-25 03:18:34 -0600

    Comment out useAuth to make program run

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,6 +1,6 @@
 import React, { useRef } from 'react'
 import {Form, Button, Card} from 'react-bootstrap'
-import { useAuth } from "../components/contexts/AuthContext";
+//import { useAuth } from "../components/contexts/AuthContext";
 
 
 export default function Signup() {
