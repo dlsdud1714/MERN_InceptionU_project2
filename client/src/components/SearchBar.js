f9d345110b396d2394e70d090a4f6f76483c1298
Derick Young <young.derickj@gmail.com>
2022-04-26 11:21:46 -0600

    Update Searchbar
    Styling almost finished. Virtualized list is displaying but it renders a list for every time it maps.

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,53 +1,82 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../App.css";
+import Box from '@mui/material/Box';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
 
 const SearchBarDropDown = (props) => {
   const val = props.val;
+  let navigate = useNavigate()
   return (
-    <div className="search-bar-dropdown">
-      <div class="list-group">
-        <a href={`/business/${val._id}`} className="list-group-item list-group-item-action">
-            {val.title}
-        </a>
-      </div>
-    </div>
-
+    <Box sx={{ width: '100%', height: 50, maxWidth: 360, bgcolor: 'background.paper', border: '1px solid grey' }}>
+      <FixedSizeList
+        height={400}
+        width={360}
+        itemSize={46}
+        itemCount={200}
+        overscanCount={5}
+      >
+    {() => <ListItem component="div" disablePadding>
+      <ListItemButton onClick={() => navigate(`/business/${val._id}`) }>
+        <ListItemText primary={val.title}/>
+      </ListItemButton>
+    </ListItem>}
+    </FixedSizeList>
+    </Box>
   );
 };
+
+    // <div className="search-bar-dropdown">
+    //   <div class="list-group">
+    //     <a href={`/business/${val._id}`} className="list-group-item list-group-item-action">
+    //         {val.title}
+    //     </a>
+    //   </div>
+    // </div>
 
 const SearchBar = (props) => {
   // console.log("props is:", props)
   // console.log("businessData is:", businessData)
   const [searchTerm, setSearchTerm] = useState("");
   const businessData = props.businessData;
+  let mappedData = {}
   return (
     <div className="search-bar-dropdown">
       <input type="text" className="form-control" placeholder="Search" onChange={(event) => setSearchTerm(event.target.value)} />
-      {businessData &&
-        businessData
-        .filter((value) => {
+      {businessData&&businessData.filter((value) => {
           if (searchTerm === "") return false;
           else if (
             value?.title?.toLowerCase().includes(searchTerm.toLowerCase())
             )
             return true;
-          })
-          .map((val) => {
-            return (<SearchBarDropDown  val = {val}/>
-            );
-          })}
+          }).map((val) => {
+            return (  
+              <SearchBarDropDown val={val}/>
+              );
+            })}
+          
     </div>
   );
 };
 
-//import { Link } from "react-router-dom";
-// <Link to={`/business/${val._id}`}>
-//   <ul className="searchResult">
-//     <a href={`/business/${val._id}`}> {val.title}</a>
-//   </ul>
-// </Link>
+            
+
+// remove the .map and send back only the filtered array
+
+// or
+
+// push each val from .map into a new array
+
+//<SearchBarDropDown val={val}/> 
+
+
+
 // use .filter() to filter data
 // then .map() to render the renaming entries in the array
 // used useState to set and rerender the page when the searchTerm is updated
