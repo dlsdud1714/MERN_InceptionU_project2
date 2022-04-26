ea3cbe06f2a5186615f9305f4e03f6b9a2cc7f0e
Nicole <nicoleamdubois@hotmail.com>
2022-04-25 23:24:43 -0600

    fixed bugs

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -34,13 +34,13 @@ const Header = (props) => {
               <NavDropdown title="Categories" id="navbarScrollingDropdown">
                 <NavDropdown.Item href="/categories/auto"> Auto Service </NavDropdown.Item>
                 <NavDropdown.Item href="/categories/beauty"> Beauty </NavDropdown.Item>
-                <NavDropdown.Item href="/categories/cafe"> Cafe </NavDropdown.Item>
+                <NavDropdown.Item href="/categories/coffeeshop"> Cafe </NavDropdown.Item>
                 <NavDropdown.Item href="/categories/clothing"> Clothing </NavDropdown.Item>
                 <NavDropdown.Item href="/categories/fastfood"> Fastfood </NavDropdown.Item>
                 <NavDropdown.Item href="/categories/groceries"> Groceries </NavDropdown.Item>
                 <NavDropdown.Item href="/categories/pets"> Pets </NavDropdown.Item>
                 <NavDropdown.Item href="/categories/recreation"> Recreation </NavDropdown.Item>
-                <NavDropdown.Item href="/categories/restaurants"> Restaurants </NavDropdown.Item>
+                <NavDropdown.Item href="/categories/restaurant"> Restaurants </NavDropdown.Item>
               </NavDropdown>
             </Nav>
               <SearchBar businessData={businessData}/>
