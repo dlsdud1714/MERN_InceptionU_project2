21a1f0af9eb5623b88f594ed95fe8a53060578e7
Nicole <nicoleamdubois@hotmail.com>
2022-04-26 22:25:46 -0600

    bug fix

--- client/src/components/BusinessDetailCards.js
+++ client/src/components/BusinessDetailCards.js
@@ -5,11 +5,16 @@ export const BusinessDetailCards = (props) => {
  const data = props.businessdata
  console.log("what is data?", data )
   
-
-
   return (
     <div>
-      <div>{data&&data.title}</div>
+      <div>{data&&data.imgUrl}</div>
+      <div> Customer Saticfaction Rating: {data&&data.rating}</div>
+      <div>Business Address: {data&&data.address}</div>
+      <div>Phone Number: {data&&data.phoneNumber}</div>
+      <div>City Quadrant: {data&&data.quadrant}</div>
+      <div>Website: {data&&data.website}</div>
+      <div>Neighborhood: {data&&data.neighborhood}</div>
+      <div>Business Status: {data&&data.jobStatus}</div>
     </div>
   )
 }
