7c63eaed6067f6e6599bcd8d62069fce587ecd48
Nicole <nicoleamdubois@hotmail.com>
2022-04-27 22:52:22 -0600

    Added onClick for URL's

--- client/src/components/BusinessDetailCards.js
+++ client/src/components/BusinessDetailCards.js
@@ -19,7 +19,7 @@ export const BusinessDetailCards = (props) => {
       <img src={data && data.imgUrl} alt="" height={350}
           width={500}/>
       <div> Customer Saticfaction Rating: {data && data.rating}</div>
-      <div>Business Address: {data && data.address}</div>
+      <div>Business Address: <button onclick= {data && data.address}>Click Me</button></div>
       <div>Phone Number: {data && data.phoneNumber}</div>
       <div>City Quadrant: {data && data.quadrant}</div>
       <div>Website: {data && data.website}</div>
