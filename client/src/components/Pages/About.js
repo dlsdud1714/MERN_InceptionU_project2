c6da2649008e6f90b41218af2cfff59657da507c
Nicole <nicoleamdubois@hotmail.com>
2022-05-02 16:38:15 -0600

    Trial and Error for card on page

--- client/src/components/Pages/About.js
+++ client/src/components/Pages/About.js
@@ -1,8 +1,30 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
+// import Aboutcard from "../Aboutcard";
 
 const About = () => {
   return <div>About</div>;
+};
+
+const Aboutcard = () => {
+  console.log(about);
+  return (
+    <>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Text>
+            "Welcome to our project 2! Our website was developed with Calgary
+            local businesses in mind. We wanted to create a platform that is
+            easily accessable to users and businesses. There are many features
+            that help create an easy and positive user experience. Features
+            include: GPS with locationsnear user, user and business login pages,
+            business catergories, and interective business posting section. "
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </>
+  );
 };
 
 // const aboutUs = () => {
@@ -17,3 +39,4 @@ const About = () => {
 // };
 
 export default About;
+Aboutcard;
