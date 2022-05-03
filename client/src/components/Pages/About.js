5faab54a65357292726e1b045032db347ce6e368
Nicole <nicoleamdubois@hotmail.com>
2022-05-03 14:20:56 -0600

    Added picture and template to page

--- client/src/components/Pages/About.js
+++ client/src/components/Pages/About.js
@@ -1,10 +1,26 @@
-import React from "react";
-// import { useParams } from "react-router-dom";
-// import Aboutcard from "../Aboutcard";
+import React from 'react';
+import { MDBCard, MDBCardBody, MDBCardText, MDBCardImage } from 'mdb-react-ui-kit';
 
-const About = () => {
-  return <div>About</div>;
-};
+export default function App() {
+  return (
+    <MDBCard style={{ width: '18rem' }}>
+      <MDBCardImage src='https://cdn.pixabay.com/photo/2020/05/30/04/52/calgary-5237661_960_720.jpg' alt='...' position='top' />
+      <MDBCardBody>
+        <MDBCardText>
+          Some quick example text to build on the card title and make up the bulk of the card's content.
+        </MDBCardText>
+      </MDBCardBody>
+    </MDBCard>
+  );
+}
+
+// import React from "react";
+// // import { useParams } from "react-router-dom";
+// // import Aboutcard from "../Aboutcard";
+
+// const About = () => {
+//   return <div>About</div>;
+// };
 
 // const Aboutcard = () => {
 //   console.log(about);
