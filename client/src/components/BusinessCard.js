dcbed1e019db77e374416baf4a1f3710454873f8
Nicole <nicoleamdubois@hotmail.com>
2022-05-04 15:16:16 -0600

    clean code

--- client/src/components/BusinessCard.js
+++ client/src/components/BusinessCard.js
@@ -6,23 +6,6 @@ const BusinessCard = (props) => {
   const filterData = props.Data;
   console.log(filterData);
 
-//   <Row xs={1} md={2} className="g-4">
-//   {Array.from({ length: 4 }).map((_, idx) => (
-//     <Col>
-//       <Card>
-//         <Card.Img variant="top" src="holder.js/100px160" />
-//         <Card.Body>
-//           <Card.Title>Card title</Card.Title>
-//           <Card.Text>
-//             This is a longer card with supporting text below as a natural
-//             lead-in to additional content. This content is a little bit longer.
-//           </Card.Text>
-//         </Card.Body>
-//       </Card>
-//     </Col>
-//   ))}
-// </Row>
-
   return (
     <div className="card">
       <Container className='p-4'>
