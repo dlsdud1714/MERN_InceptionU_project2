c97f25663e2b0fe376264d89c26cfea678bc0920
Nicole <nicoleamdubois@hotmail.com>
2022-05-04 14:26:07 -0600

    Edited card

--- client/src/components/Aboutcard.js
+++ client/src/components/Aboutcard.js
@@ -1,22 +1,36 @@
 import React from "react";
+import { Card } from "react-bootstrap";
 
 const Aboutcard = (props) => {
-    const card = props.Aboutcard;
-    console.log(about);
-    return(
-    <Card border="primary" style={{ width: '18rem' }}>
-        <Card.Header>About YYCWhatsNearMe?</Card.Header>
-        <Card.Body>
-          <Card.Text>
-            <h6>Welcome to our project 2! Our website was developed with Calgary
-            local businesses in mind. We wanted to create a platform that is
+  const card = props.Aboutcard;
+  return (
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px180" />
+    <Card.Header>About YYCWhatsNearMe?</Card.Header>
+    <Card.Body>
+      <Card.Text>
+      Welcome to YYCWhatsNearMe??(project 2)! Our website has been developed with Calgary
+            local businesses in mind. We created a platform that is
             easily accessable to users and businesses. There are many features
-            that help create an easy and positive user experience. Features
-            include: GPS with locationsnear user, user and business login pages,
-            business catergories, and interective business posting section.</h6>
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    )};
+            that help create an easy and positive user and business owner experience. Features
+            include: GPS with locations near user, user and business login pages,
+            business catergories, and interective business posting section.
+      </Card.Text>
+    </Card.Body>
+  </Card>
+    /* <Card border="primary" style={{ width: "86rem" }}>
+      
+      <Card.Body>
+        <Card.Text>
+          <h6>
+            
+          </h6>
+        </Card.Text>
+      </Card.Body>
+    </Card> */
+  );
+};
 
-export default Aboutcard; 
+
+
+export default Aboutcard;
