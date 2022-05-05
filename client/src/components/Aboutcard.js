b58a2db27fe7aa47191efaa7e001f077b9cd240e
Nicole <nicoleamdubois@hotmail.com>
2022-05-05 12:43:27 -0600

    Added picture and format

--- client/src/components/Aboutcard.js
+++ client/src/components/Aboutcard.js
@@ -5,19 +5,22 @@ const Aboutcard = (props) => {
   const card = props.Aboutcard;
   return (
   <Card>
-    <Card.Header>About YYCWhatsNearMe?</Card.Header>
+
+    <Card.Header>About YYCWhatsNearMe?</Card.Header>    
+    {/* // "https://moradmedia.com/get/files/image/galleries/Web_Design_Firms_Calgary_Inglewood_header-0001.jpg?1400x1400"  */}
+       
     <Card.Body>
       <Card.Text>
       Welcome to YYCWhatsNearMe??(project 2)! Our website has been developed with Calgary
-            local businesses in mind. We created a platform that is
-            easily accessable to users and businesses. There are many features
-            that help create an easy and positive user and business owner experience. Features
-            include: GPS with locations near user, user and business login pages,
-            business catergories, and interective business posting section.
-        <Card.Img variant="bottom" height={240} src="https://th.bing.com/th/id/OIP.vhyPTcL1HL_J6MoWo-DpXAHaEK?w=305&h=180&c=7&r=0&o=5&pid=1.7" />
-        
-
+      local businesses in mind. We created a platform that is
+      easily accessable to users and businesses. There are many features
+      that help create an easy and positive user and business owner experience. Features
+      include: GPS with locations near user, user and business login pages,
+      business catergories, and interective business posting section.
+      
       </Card.Text>
+    <Card.Img variant="bottom"  style={{width: "75%"}} src="https://th.bing.com/th/id/OIP.ukyHlTJj4ZEFZ3b9yr4ZbwHaDk?pid=ImgDet&rs=1" />
+
     </Card.Body>
   </Card>
 
